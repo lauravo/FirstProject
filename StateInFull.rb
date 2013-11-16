62fa046ea8f293ce5ce1329e75f486f97c7cce4d
Laura Vo <laura.g.vo@gmail.com>
2013-11-16 12:35:16 -0800

    Refactor

--- StateInFull.rb
+++ StateInFull.rb
@@ -1,92 +1,94 @@
 class StateInFull
 
-		def initialize 
-			@state_abbr = {
-				  'AL' => 'Alabama',
-				  'AK' => 'Alaska',
-				  'AS' => 'America Samoa',
-				  'AZ' => 'Arizona',
-				  'AR' => 'Arkansas',
-				  'CA' => 'California',
-				  'CO' => 'Colorado',
-				  'CT' => 'Connecticut',
-				  'DE' => 'Delaware',
-				  'DC' => 'District of Columbia',
-				  'FM' => 'Micronesia1',
-				  'FL' => 'Florida',
-				  'GA' => 'Georgia',
-				  'GU' => 'Guam',
-				  'HI' => 'Hawaii',
-				  'ID' => 'Idaho',
-				  'IL' => 'Illinois',
-				  'IN' => 'Indiana',
-				  'IA' => 'Iowa',
-				  'KS' => 'Kansas',
-				  'KY' => 'Kentucky',
-				  'LA' => 'Louisiana',
-				  'ME' => 'Maine',
-				  'MH' => 'Islands1',
-				  'MD' => 'Maryland',
-				  'MA' => 'Massachusetts',
-				  'MI' => 'Michigan',
-				  'MN' => 'Minnesota',
-				  'MS' => 'Mississippi',
-				  'MO' => 'Missouri',
-				  'MT' => 'Montana',
-				  'NE' => 'Nebraska',
-				  'NV' => 'Nevada',
-				  'NH' => 'New Hampshire',
-				  'NJ' => 'New Jersey',
-				  'NM' => 'New Mexico',
-				  'NY' => 'New York',
-				  'NC' => 'North Carolina',
-				  'ND' => 'North Dakota',
-				  'OH' => 'Ohio',
-				  'OK' => 'Oklahoma',
-				  'OR' => 'Oregon',
-				  'PW' => 'Palau',
-				  'PA' => 'Pennsylvania',
-				  'PR' => 'Puerto Rico',
-				  'RI' => 'Rhode Island',
-				  'SC' => 'South Carolina',
-				  'SD' => 'South Dakota',
-				  'TN' => 'Tennessee',
-				  'TX' => 'Texas',
-				  'UT' => 'Utah',
-				  'VT' => 'Vermont',
-				  'VI' => 'Virgin Island',
-				  'VA' => 'Virginia',
-				  'WA' => 'Washington',
-				  'WV' => 'West Virginia',
-				  'WI' => 'Wisconsin',
-				  'WY' => 'Wyoming'
-				}
-			@state_full = @state_abbr.invert
+@@state_abbr = {
+		  'AL' => 'Alabama',
+		  'AK' => 'Alaska',
+		  'AS' => 'America Samoa',
+		  'AZ' => 'Arizona',
+		  'AR' => 'Arkansas',
+		  'CA' => 'California',
+		  'CO' => 'Colorado',
+		  'CT' => 'Connecticut',
+		  'DE' => 'Delaware',
+		  'DC' => 'District of Columbia',
+		  'FM' => 'Micronesia1',
+		  'FL' => 'Florida',
+		  'GA' => 'Georgia',
+		  'GU' => 'Guam',
+		  'HI' => 'Hawaii',
+		  'ID' => 'Idaho',
+		  'IL' => 'Illinois',
+		  'IN' => 'Indiana',
+		  'IA' => 'Iowa',
+		  'KS' => 'Kansas',
+		  'KY' => 'Kentucky',
+		  'LA' => 'Louisiana',
+		  'ME' => 'Maine',
+		  'MH' => 'Islands1',
+		  'MD' => 'Maryland',
+		  'MA' => 'Massachusetts',
+		  'MI' => 'Michigan',
+		  'MN' => 'Minnesota',
+		  'MS' => 'Mississippi',
+		  'MO' => 'Missouri',
+		  'MT' => 'Montana',
+		  'NE' => 'Nebraska',
+		  'NV' => 'Nevada',
+		  'NH' => 'New Hampshire',
+		  'NJ' => 'New Jersey',
+		  'NM' => 'New Mexico',
+		  'NY' => 'New York',
+		  'NC' => 'North Carolina',
+		  'ND' => 'North Dakota',
+		  'OH' => 'Ohio',
+		  'OK' => 'Oklahoma',
+		  'OR' => 'Oregon',
+		  'PW' => 'Palau',
+		  'PA' => 'Pennsylvania',
+		  'PR' => 'Puerto Rico',
+		  'RI' => 'Rhode Island',
+		  'SC' => 'South Carolina',
+		  'SD' => 'South Dakota',
+		  'TN' => 'Tennessee',
+		  'TX' => 'Texas',
+		  'UT' => 'Utah',
+		  'VT' => 'Vermont',
+		  'VI' => 'Virgin Island',
+		  'VA' => 'Virginia',
+		  'WA' => 'Washington',
+		  'WV' => 'West Virginia',
+		  'WI' => 'Wisconsin',
+		  'WY' => 'Wyoming'
+		}
+	@@state_full = @@state_abbr.invert
 
-	end
+		
 
 	def get_full(acronym)
 
-		acr = acronym.upcase
+		# acr = acronym.upcase
 
-		if @state_abbr.has_key?(acr)
-			return @state_abbr[acr]
-		else
-			return "No Match Found, Check Spelling."
-		end
+		# if @state_abbr.has_key?(acr)
+		# 	return @state_abbr[acr]
+		# else
+		# 	return "No Match Found, Check Spelling."
+		# end
+
+		@@state_abbr[acronym.upcase] ||  "No Match Found, check spelling"
 
 	end
 
 	def get_abbr(state)
 
-		full = state.capitalize
+		# full = state.capitalize
 
-		if @state_full.has_key?(full)
-			return @state_full[full]
-		else
-			return "No Match Found, Check Spelling"
-		end
+		# if @state_full.has_key?(full)
+		# 	return @state_full[full]
+		# else
+		# 	return "No Match Found, Check Spelling"
+		# end
 
+		@@state_full[state.capitalize] ||  "No Match Found, check spelling"
 
 	end
 
@@ -94,7 +96,8 @@ class StateInFull
 end
 
 state = StateInFull.new
-name = state.get_full('Wa')
+state2 = StateInFull.new
+name = state.get_full('zz')
 puts name
 name2 = state.get_abbr('alabama')
 puts name2
